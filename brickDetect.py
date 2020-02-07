8b943c4a0d39b757b5f5cea174496e624f7d26c8
NNK <nk1701@protonmail.com>
2020-02-06 19:36:12 -0600

    Modify brickDetect.py to use LegoDetector class

--- brickDetect.py
+++ brickDetect.py
@@ -1,52 +1,22 @@
 
-import cv2
-import numpy as np
-import xlrd
 import xlwt
 from time import sleep
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+from legodetector import LegoDetector
 
-class LegoDetector:
+def main():
+    camIndex = 0
+    resolution = (1280, 720)  #ASSUMING PI CAM V2
+    xl = "colors.xlsx"
+
+    detector = LegoDetector(camIndex, resolution, xl)
+    sleep(2)
+
+    frame = detector.snap()
     """
-    This is a class for detecting legos using a Raspberry Pi Camera
+    for None: #TODO: iterate through all colors in the excel file and run the detection pipeline
+        detector.pipeline(frame, None)
+        sleep(0.5)
     """
-
-    def __init__(self, camera, res):
-        """
-        The constructor for the LegoDetector Class
-
-        Parameters:
-            camera (int): camera port
-            res (tuple): camera resolution
-        """
-        
-        self.camera = PiCamera(camera)
-        self.camera.resolution = (res)
-        self.camera.start_preview()
-        self.rawCapture = PiRGBArray(self.camera, size=res)
-        sleep(2)
-        print("Camera initialized")
-
-    def snap(self):
-        """
-        Capture a still image
-        
-        Returns:
-            image: A file with all the image data
-        """
-
-        self.rawCapture.truncate(0)
-        self.camera.capture(self.rawCapture, format="bgr")
-        image = self.rawCapture.array
-        return image
-
-    def pipeline(self, frame):
-        """
-        Run the image processing pipeline to detect bricks
-
-        Parameters:
-            frame (image): Input image frame 
-        """
-
-        
+    detector.processBricks()
+    frame = detector.drawContours(frame)
+    detector.showContours(frame)
