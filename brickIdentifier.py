09f34dbb69cc409a65d9b7a3f5b9b6f4158e900b
narenkasinath <kasinatnn@gmail.com>
2020-03-03 19:42:55 -0600

    Use new identification_pipeline

--- brickIdentifier.py
+++ brickIdentifier.py
@@ -1,5 +1,18 @@
-
 from time import sleep
 from legodetector import LegoDetector
-import cv2
-import numpy
+
+
+def main():
+    camIndex = 0
+    resolution = (1280, 720)  # ASSUMING PI CAM V2
+    xl = "colors.xlsx"
+
+    detector = LegoDetector(camIndex, resolution, xl)
+    sleep(2)
+
+    frame = detector.snap()
+    detector.identification_pipeline(frame)
+
+
+if __name__ == '__main__':
+    main()
